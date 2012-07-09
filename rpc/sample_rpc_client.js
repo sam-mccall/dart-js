392bf66417d7bda67d2ed6c8d9fe64311e7e993b
Sam McCall <sammccall@google.com>
2012-07-09 17:35:37 +0200

    oops, dont rely on optional features

--- rpc/sample_rpc_client.js
+++ rpc/sample_rpc_client.js
@@ -1,7 +1,8 @@
 window.addEventListener('load', function() {
 
 var calc = new rpc.Client('calculator').connect();
-assertEquals(20, calc.multiply(4, calc.add(2, 3)));
+//assertEquals(20, calc.multiply(4, calc.add(2, 3)));
+assertEquals(20, calc.method('multiply')(4, calc.method('add')(2, 3)));
 console.log('success!');
 
 function assertEquals(a, b) {
